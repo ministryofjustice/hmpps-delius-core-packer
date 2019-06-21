5922c4fd4c68fd44733db64bb0f355fcd4050b68
John Barber <jsbarber@gmail.com>
2019-06-21 16:33:41 +0100

    wip

--- scripts/windows/iaps/GetS3SetupArtefacts.ps1
+++ scripts/windows/iaps/GetS3SetupArtefacts.ps1
@@ -4,8 +4,7 @@ $VerbosePreference="Continue"
 $keyPrefix = "IAPS/"
 # The local file path where files should be copied
 $localPath = "C:\Setup\"
-# Debug
-Get-ChildItem env:
+
 Read-S3Object -BucketName $env:ZAIZI_BUCKET -KeyPrefix $keyPrefix -Folder $localPath
 
 if( (Get-ChildItem $localPath | Measure-Object).Count -eq 0)
