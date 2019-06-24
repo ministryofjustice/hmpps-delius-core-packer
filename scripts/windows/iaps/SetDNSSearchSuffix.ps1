f4f43de544116954e6f1fba2c9668b2b4976a049
John Barber <jsbarber@gmail.com>
2019-06-24 16:24:08 +0100

    resolve escape issue

--- scripts/windows/iaps/SetDNSSearchSuffix.ps1
+++ scripts/windows/iaps/SetDNSSearchSuffix.ps1
@@ -5,17 +5,25 @@ $VerbosePreference = "Continue"
 # add this environment's internal r53 zone suffix to the global search list at runtime
 
 # Create a startup job to update dns search suffix with internal domain name
-if ( (Get-ScheduledJob -Name UpdateDNSSearchSuffix | Measure-Object).Count -eq 0 ) {
-    $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-    Register-ScheduledJob -Trigger $trigger -FilePath C:\Setup\SetDNSSearchSuffix.ps1 -Name UpdateDNSSearchSuffix
-}
+try {
+    if ( (Get-ScheduledJob -Name UpdateDNSSearchSuffix | Measure-Object).Count -eq 0 ) {
+        $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+        Register-ScheduledJob -Trigger $trigger -FilePath C:\Setup\SetDNSSearchSuffix.ps1 -Name UpdateDNSSearchSuffix
+    } else {
+        Write-Host ('Existing trigger job found.')
+    }
 
-# Get the instance id from ec2 meta data
-$instanceid = Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id | Select-Object -ExpandProperty Content
-# Get the environment name from this instance's environment-name tag value
-$environment = aws ec2 describe-tags --region eu-west-2 --filters "Name=resource-id,Values=$instanceid","Name=key,Values=environment-name" --query 'Tags[0].Value' --output text
-# Add the internal hosted zone suffix to the existing list
-$dnsconfig = Get-DnsClientGlobalSetting
-$dnsconfig.SuffixSearchList += "$environment.internal"
-Set-DnsClientGlobalSetting -SuffixSearchList $dnsconfig.SuffixSearchList
-Clear-DnsClientCache
+    # Get the instance id from ec2 meta data
+    $instanceid = Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id | Select-Object -ExpandProperty Content
+    # Get the environment name from this instance's environment-name tag value
+    $environment = aws ec2 describe-tags --region eu-west-2 --filters "Name=resource-id,Values=$instanceid","Name=key,Values=environment-name" --query 'Tags[0].Value' --output text
+    # Add the internal hosted zone suffix to the existing list
+    $dnsconfig = Get-DnsClientGlobalSetting
+    $dnsconfig.SuffixSearchList += "$environment.internal"
+    Set-DnsClientGlobalSetting -SuffixSearchList $dnsconfig.SuffixSearchList
+    Clear-DnsClientCache
+}
+catch {
+    Write-Host ('Failed to Update DNS Search Suffix List')
+    exit 1
+}
--- scripts/windows/iaps/SetupOracleClient.ps1
+++ scripts/windows/iaps/SetupOracleClient.ps1
@@ -1,45 +1,45 @@
-$ErrorActionPreference = "Stop"
-$VerbosePreference = "Continue"
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
 
 # Extract setup files from 7z archive
 try {
-    New-Item -Path "C:\Setup\Oracle" -Name "Install" -ItemType "directory"
-    Start-Process "$env:ProgramFiles\7-Zip\7z.exe" "x -oC:\Setup\Oracle\Install C:\Setup\Oracle\Oracle_12c_Win32_12.1.0.2.0.7z" -Wait -Verb RunAs
+    New-Item -Path 'C:\Setup\Oracle' -Name 'Install' -ItemType 'directory'
+    Start-Process $env:ProgramFiles\7-Zip\7z.exe 'x -oC:\Setup\Oracle\Install C:\Setup\Oracle\Oracle_12c_Win32_12.1.0.2.0.7z' -Wait -Verb RunAs
 }
 catch {
-    Write-Host ("Failed to extract Oracle client setup using 7z")
+    Write-Host ('Failed to extract Oracle client setup using 7z')
     exit 1
 }
 # Add required patches for x86 client
 # Edit oaparam file to use 32bit vcredist
 try {
-    $oaparamfile = "C:\Setup\Oracle\Install\Oracle_12c_Win32_12.1.0.2.0\client32\install\oraparam.ini"
+    $oaparamfile = 'C:\Setup\Oracle\Install\Oracle_12c_Win32_12.1.0.2.0\client32\install\oraparam.ini'
     if (Test-Path -Path $oaparamfile) {
         ((Get-Content -path $oaparamfile -Raw) -replace 'MSVCREDIST_LOC=vcredist_x64.exe','MSVCREDIST_LOC=vcredist_x86.exe') | Set-Content -Path $oaparamfile 
     } else {
-        write-host("Error - could not find oracle setup param file: $oaparamfile")
+        write-host('Error - could not find oracle setup param file: $oaparamfile')
         exit 1
     }
     # Create x86 reg entry
     Push-Location
     Set-Location HKLM:
     New-Item -Path .\SOFTWARE\Wow6432Node -Name ORACLE -Type Directory –Force
-    New-Itemproperty -Path .\SOFTWARE\Wow6432Node\ORACLE -Name 'inst_loc' -Value 'C:\Program Files (x86)\Oracle\Inventory' -PropertyType "String"
+    New-Itemproperty -Path .\SOFTWARE\Wow6432Node\ORACLE -Name 'inst_loc' -Value 'C:\Program Files (x86)\Oracle\Inventory' -PropertyType 'String'
     Pop-Location
 }
 catch {
-    Write-Host ("Failed creating x86 registry entries")
+    Write-Host ('Failed creating x86 registry entries')
     exit 1
 }
 
 # Install 32bit client from answer file, using runas administrator verb
 # See: https://docs.oracle.com/database/121/NTCLI/advance.htm#NTCLI1347
 try {
-    $oracleanswerfile = "C:\Setup\Oracle\OracleClient.rsp"
-    Start-Process -FilePath "C:\Setup\Oracle\Install\Oracle_12c_Win32_12.1.0.2.0\client32\setup.exe" -Verb RunAs "ORACLE_HOSTNAME=$env:computername" -ArgumentList "-silent -nowelcome -nowait -noconfig -responseFile $oracleanswerfile" -Wait
+    $oracleanswerfile = 'C:\Setup\Oracle\OracleClient.rsp'
+    Start-Process -FilePath 'C:\Setup\Oracle\Install\Oracle_12c_Win32_12.1.0.2.0\client32\setup.exe' -Verb RunAs 'ORACLE_HOSTNAME=$env:computername' -ArgumentList '-silent -nowelcome -nowait -noconfig -responseFile $oracleanswerfile' -Wait
 }
 catch {
-    Write-Host ("Failed installing Oracle Client")
+    Write-Host ('Failed installing Oracle Client')
     exit 1
 }
 # Configure net connections
@@ -49,6 +49,6 @@ catch {
 #     Copy-Item 'C:\Setup\Oracle\tnsnames.ora.tmpl' -Destination $tnsnameorafile
 # }
 # catch {
-#     Write-Host ("Error - Failed to create ora file: $tnsnameorafile")
+#     Write-Host ('Error - Failed to create ora file: $tnsnameorafile')
 #     exit 1
 # }
