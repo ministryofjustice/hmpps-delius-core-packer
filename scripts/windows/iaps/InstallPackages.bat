8f3d19f9bfb323086330b97316a94a2994b61126
John Barber <jsbarber@gmail.com>
2019-06-26 14:04:15 +0100

    Correct .bat file comments preventing .net install with chocolatey

--- scripts/windows/iaps/InstallPackages.bat
+++ scripts/windows/iaps/InstallPackages.bat
@@ -1,17 +1,17 @@
-# Chocolatey installed on base image
+REM Chocolatey installed on base image
 
-# .Net packages
+REM DotNet packages
 choco install -y dotnet3.5 --version 3.5.20160716
 
-# vcredist packages
+REM vcredist packages
 choco install -y vcredist2010
 choco install -y vcredist2008
 
-# nginx proxy as a service
+REM nginx proxy as a service
 choco install -y nginx-service --version 1.16.0
 
-# Firefox browser
+REM Firefox browser
 choco install -y firefox --version 67.0.3
 
-# 7Zip archive util
+REM 7Zip archive util
 choco install -y 7zip.install --version 19.0
