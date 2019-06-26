d1d109e0ce18f2b501310294a0e82d750912eecb
John Barber <jsbarber@gmail.com>
2019-06-26 14:29:55 +0100

    batch check exit codes

--- scripts/windows/iaps/InstallPackages.bat
+++ scripts/windows/iaps/InstallPackages.bat
@@ -2,16 +2,22 @@ REM Chocolatey installed on base image
 
 REM DotNet packages
 choco install -y dotnet3.5 --version 3.5.20160716
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM vcredist packages
 choco install -y vcredist2010
+if %errorlevel% neq 0 exit /b %errorlevel%
 choco install -y vcredist2008
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM nginx proxy as a service
 choco install -y nginx-service --version 1.16.0
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Firefox browser
 choco install -y firefox --version 67.0.3
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM 7Zip archive util
 choco install -y 7zip.install --version 19.0
+if %errorlevel% neq 0 exit /b %errorlevel%
